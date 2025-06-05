1e009482de030ca8cf00de52cd3d45ae568b6c2d
guilhermebernava <guilhermebernava00@gmail.com>
2025-06-05 16:28:11 -0300

    add optimization

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,53 @@
 import { Route, Routes, Navigate } from "react-router-dom";
-import Product from "./pages/Product/Product";
-import Home from "./pages/Home/Home";
-import NotFound from "./pages/NotFound/NotFound";
-import Pricing from "./pages/Pricing/Pricing";
-import Login from "./pages/Login/Login";
-import Logged from "./pages/Logged/Logged";
-import Cities from "./components/Cities/Cities";
-import ProtectedRoute from "./pages/ProtectedRoute";
 import { CitiesProvider } from "./context/CitiesContext";
+import SpinnerFullPage from "./components/SpinnerFullPage/SpinnerFullPage";
+
 import "./App.css";
-import FormModal from "./components/FormModal/FormModal";
-import City from "./components/City/City";
-import Countries from "./components/Countries/Countries";
+import { lazy, Suspense } from "react";
+
+//irao carregar conforme chamado, isso melhora o BUNDLE do REACT
+//que deixa a primeira vez que abrir o site melhor
+const Home = lazy(() => import("./pages/Home/Home"));
+const Product = lazy(() => import("./pages/Product/Product"));
+const NotFound = lazy(() => import("./pages/NotFound/NotFound"));
+const Pricing = lazy(() => import("./pages/Pricing/Pricing"));
+const Login = lazy(() => import("./pages/Login/Login"));
+const Logged = lazy(() => import("./pages/Logged/Logged"));
+const Cities = lazy(() => import("./components/Cities/Cities"));
+const Countries = lazy(() => import("./components/Countries/Countries"));
+const FormModal = lazy(() => import("./components/FormModal/FormModal"));
+const City = lazy(() => import("./components/City/City"));
+const ProtectedRoute = lazy(() => import("./pages/ProtectedRoute"));
 
 function App() {
   return (
     <div className="app">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="product" element={<Product />} />
-        <Route path="pricing" element={<Pricing />} />
-        <Route path="login" element={<Login />} />
-        <Route
-          path="app"
-          element={
-            <ProtectedRoute>
-              <CitiesProvider>
-                <Logged />
-              </CitiesProvider>
-            </ProtectedRoute>
-          }
-        >
-          <Route index element={<Navigate to="cities" replace />} />
-          <Route path="cities" element={<Cities />}>
-            <Route path="city/:id" element={<City />} />
+      <Suspense fallback={<SpinnerFullPage />}>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="product" element={<Product />} />
+          <Route path="pricing" element={<Pricing />} />
+          <Route path="login" element={<Login />} />
+          <Route
+            path="app"
+            element={
+              <ProtectedRoute>
+                <CitiesProvider>
+                  <Logged />
+                </CitiesProvider>
+              </ProtectedRoute>
+            }
+          >
+            <Route index element={<Navigate to="cities" replace />} />
+            <Route path="cities" element={<Cities />}>
+              <Route path="city/:id" element={<City />} />
+            </Route>
+            <Route path="countries" element={<Countries />} />
+            <Route path="formModal/:lat/:lng" element={<FormModal />} />
           </Route>
-          <Route path="countries" element={<Countries />} />
-          <Route path="formModal/:lat/:lng" element={<FormModal />} />
-        </Route>
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 }
